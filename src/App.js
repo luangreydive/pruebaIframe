82a92a37855861d6e07facc5733064ed9d3e1223
Ezequiel <ezepuglisi@gmail.com>
2022-12-27 16:25:28 -0300

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src='https://www.google.com' title='hola' width={'100%'} height={'800px'} />
+    </div>
+  );
+}
+
+export default App;
